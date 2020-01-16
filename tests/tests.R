57847c06439bc476a77ebec671e0c8aa44d38efa
ROBIN Yoann <yoann.robin.k@gmail.com>
2020-01-16 12:26:19 +0100

    Add generic test file

---
+++ tests/tests.R
@@ -0,0 +1,223 @@
+
+##################################################################################
+##################################################################################
+##                                                                              ##
+## Copyright Yoann Robin, 2020                                                  ##
+##                                                                              ##
+## yoann.robin.k@gmail.com                                                      ##
+##                                                                              ##
+## This software is a computer program that is part of the ROOPSD (R Object     ##
+## Oriented Programming for Statistical Distribution). This library makes it    ##
+## possible to perform bias to  draw, fit and access to CDF and SF function for ##
+## for classic statistical distribution.                                        ##
+##                                                                              ##
+## This software is governed by the CeCILL-C license under French law and       ##
+## abiding by the rules of distribution of free software.  You can  use,        ##
+## modify and/ or redistribute the software under the terms of the CeCILL-C     ##
+## license as circulated by CEA, CNRS and INRIA at the following URL            ##
+## "http://www.cecill.info".                                                    ##
+##                                                                              ##
+## As a counterpart to the access to the source code and  rights to copy,       ##
+## modify and redistribute granted by the license, users are provided only      ##
+## with a limited warranty  and the software's author,  the holder of the       ##
+## economic rights,  and the successive licensors  have only  limited           ##
+## liability.                                                                   ##
+##                                                                              ##
+## In this respect, the user's attention is drawn to the risks associated       ##
+## with loading,  using,  modifying and/or developing or reproducing the        ##
+## software by the user in light of its specific status of free software,       ##
+## that may mean  that it is complicated to manipulate,  and  that  also        ##
+## therefore means  that it is reserved for developers  and  experienced        ##
+## professionals having in-depth computer knowledge. Users are therefore        ##
+## encouraged to load and test the software's suitability as regards their      ##
+## requirements in conditions enabling the security of their systems and/or     ##
+## data to be ensured and,  more generally, to use and operate it in the        ##
+## same conditions as regards security.                                         ##
+##                                                                              ##
+## The fact that you are presently reading this means that you have had         ##
+## knowledge of the CeCILL-C license and that you accept its terms.             ##
+##                                                                              ##
+##################################################################################
+##################################################################################
+
+##################################################################################
+##################################################################################
+##                                                                              ##
+## Copyright Yoann Robin, 2020                                                  ##
+##                                                                              ##
+## yoann.robin.k@gmail.com                                                      ##
+##                                                                              ##
+## Ce logiciel est un programme informatique faisant partie de la librairie     ##
+## ROOPSD (R Object Oriented Programming for Statistical Distribution). Cette   ##
+## librarie permet de tirer aléatoirement, inférer et acceder aux fonctions CDF ##
+## et SF des distributions statistisques classiques.                            ##
+##                                                                              ##
+## Ce logiciel est régi par la licence CeCILL-C soumise au droit français et    ##
+## respectant les principes de diffusion des logiciels libres. Vous pouvez      ##
+## utiliser, modifier et/ou redistribuer ce programme sous les conditions       ##
+## de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA     ##
+## sur le site "http://www.cecill.info".                                        ##
+##                                                                              ##
+## En contrepartie de l'accessibilité au code source et des droits de copie,    ##
+## de modification et de redistribution accordés par cette licence, il n'est    ##
+## offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,    ##
+## seule une responsabilité restreinte pèse sur l'auteur du programme, le       ##
+## titulaire des droits patrimoniaux et les concédants successifs.              ##
+##                                                                              ##
+## A cet égard  l'attention de l'utilisateur est attirée sur les risques        ##
+## associés au chargement,  à l'utilisation,  à la modification et/ou au        ##
+## développement et à la reproduction du logiciel par l'utilisateur étant       ##
+## donné sa spécificité de logiciel libre, qui peut le rendre complexe à        ##
+## manipuler et qui le réserve donc à des développeurs et des professionnels    ##
+## avertis possédant  des  connaissances  informatiques approfondies.  Les      ##
+## utilisateurs sont donc invités à charger  et  tester  l'adéquation  du       ##
+## logiciel à leurs besoins dans des conditions permettant d'assurer la         ##
+## sécurité de leurs systèmes et ou de leurs données et, plus généralement,     ##
+## à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.           ##
+##                                                                              ##
+## Le fait que vous puissiez accéder à cet en-tête signifie que vous avez       ##
+## pris connaissance de la licence CeCILL-C, et que vous en avez accepté les    ##
+## termes.                                                                      ##
+##                                                                              ##
+##################################################################################
+##################################################################################
+base::rm( list = base::ls() )
+
+
+###############
+## Libraries ##
+###############
+
+library(R6)
+library(devtools)
+library(roxygen2)
+library(ggplot2)
+library(gridExtra)
+
+try(roxygen2::roxygenize("../ROOPSD"))
+devtools::load_all("../ROOPSD")
+roxygen2::roxygenize("../ROOPSD")
+devtools::load_all("../ROOPSD")
+
+
+###########################
+## Useful plot functions ##
+###########################
+
+PlotTools = R6::R6Class( "PlotTools" , ##{{{
+	
+	
+	public = list(
+	
+	###############
+	## Arguments ##
+	###############
+	
+	os = NULL,
+	
+	
+	#################
+	## Constructor ##
+	#################
+	
+	initialize = function()
+	{
+		self$os = self$get_os()
+	},
+	
+	
+	#############
+	## Methods ##
+	#############
+	
+	get_os = function()
+	{
+		sysinf = base::Sys.info()
+		if( !is.null(sysinf) )
+		{
+			os = sysinf['sysname']
+			if( os == 'Darwin' ) os = "osx"
+		}
+		else
+		{
+			## mystery machine
+			os = .Platform$OS.type
+			if( base::grepl( "^darwin"   , R.version$os ) ) os = "osx"
+			if( base::grepl( "linux-gnu" , R.version$os ) ) os = "linux"
+		}
+		invisible(tolower(os))
+	},
+	
+	new_screen = function()
+	{
+		if( self$os == "osx" )
+		{
+			grDevices::quartz()
+		}
+		if( self$os == "linux" )
+		{
+			grDevices::X11()
+		}
+	},
+	
+	wait = function()
+	{
+		while( base::names(grDevices::dev.cur()) !='null device' ) base::Sys.sleep(1)
+	}
+	
+	)
+)
+##}}}
+
+plt = PlotTools$new()
+
+
+###############
+## Functions ##
+###############
+
+## Some functions
+##===============
+
+## Test tools
+##===========
+
+## All in one
+##===========
+
+run_all_tests = function( show = FALSE )##{{{
+{
+}
+##}}}
+
+
+
+##########
+## main ##
+##########
+
+## Read command line arguments and run (or not) tests
+##================================================{{{
+
+args = commandArgs( trailingOnly = TRUE )
+args_verbose = FALSE
+args_run     = FALSE
+if( length(args) > 0 )
+{
+	for( a in args )
+	{
+		if( a == "-r" || a == "--run-all-tests" )
+			args_run = TRUE
+		if( a == "-v" || a == "--verbose" )
+			args_verbose = TRUE
+	}
+}
+
+if( args_run )
+	run_all_tests(args_verbose)
+
+##}}}
+
+plt$wait()
+print("Done")
+
