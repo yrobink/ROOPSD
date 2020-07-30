4fc6822b7344e77632e101b3654c67f485bdfc19
yrobink <yoann.robin.k@gmail.com>
2020-07-30 08:09:09 +0200

    Update documentation and add density function

--- ROOPSD/R/dist.rv_histogram.R
+++ ROOPSD/R/dist.rv_histogram.R
@@ -1,123 +1,90 @@
 
-##################################################################################
-##################################################################################
-##                                                                              ##
-## Copyright Yoann Robin, 2020                                                  ##
-##                                                                              ##
-## yoann.robin.k@gmail.com                                                      ##
-##                                                                              ##
-## This software is a computer program that is part of the ROOPSD (R Object     ##
-## Oriented Programming for Statistical Distribution). This library makes it    ##
-## possible to perform bias to  draw, fit and access to CDF and SF function for ##
-## for classic statistical distribution.                                        ##
-##                                                                              ##
-## This software is governed by the CeCILL-C license under French law and       ##
-## abiding by the rules of distribution of free software.  You can  use,        ##
-## modify and/ or redistribute the software under the terms of the CeCILL-C     ##
-## license as circulated by CEA, CNRS and INRIA at the following URL            ##
-## "http://www.cecill.info".                                                    ##
-##                                                                              ##
-## As a counterpart to the access to the source code and  rights to copy,       ##
-## modify and redistribute granted by the license, users are provided only      ##
-## with a limited warranty  and the software's author,  the holder of the       ##
-## economic rights,  and the successive licensors  have only  limited           ##
-## liability.                                                                   ##
-##                                                                              ##
-## In this respect, the user's attention is drawn to the risks associated       ##
-## with loading,  using,  modifying and/or developing or reproducing the        ##
-## software by the user in light of its specific status of free software,       ##
-## that may mean  that it is complicated to manipulate,  and  that  also        ##
-## therefore means  that it is reserved for developers  and  experienced        ##
-## professionals having in-depth computer knowledge. Users are therefore        ##
-## encouraged to load and test the software's suitability as regards their      ##
-## requirements in conditions enabling the security of their systems and/or     ##
-## data to be ensured and,  more generally, to use and operate it in the        ##
-## same conditions as regards security.                                         ##
-##                                                                              ##
-## The fact that you are presently reading this means that you have had         ##
-## knowledge of the CeCILL-C license and that you accept its terms.             ##
-##                                                                              ##
-##################################################################################
-##################################################################################
+################################################################################
+################################################################################
+##                                                                            ##
+## Copyright Yoann Robin, 2020                                                ##
+##                                                                            ##
+## yoann.robin.k@gmail.com                                                    ##
+##                                                                            ##
+## This software is a computer program that is part of the ROOPSD (R Object   ##
+## Oriented Programming for Statistical Distribution). This library makes it  ##
+## possible to perform bias to  draw, fit and access to CDF and SF function   ##
+## for classic statistical distribution.                                      ##
+##                                                                            ##
+## This software is governed by the CeCILL-C license under French law and     ##
+## abiding by the rules of distribution of free software.  You can  use,      ##
+## modify and/ or redistribute the software under the terms of the CeCILL-C   ##
+## license as circulated by CEA, CNRS and INRIA at the following URL          ##
+## "http://www.cecill.info".                                                  ##
+##                                                                            ##
+## As a counterpart to the access to the source code and  rights to copy,     ##
+## modify and redistribute granted by the license, users are provided only    ##
+## with a limited warranty  and the software's author,  the holder of the     ##
+## economic rights,  and the successive licensors  have only  limited         ##
+## liability.                                                                 ##
+##                                                                            ##
+## In this respect, the user's attention is drawn to the risks associated     ##
+## with loading,  using,  modifying and/or developing or reproducing the      ##
+## software by the user in light of its specific status of free software,     ##
+## that may mean  that it is complicated to manipulate,  and  that  also      ##
+## therefore means  that it is reserved for developers  and  experienced      ##
+## professionals having in-depth computer knowledge. Users are therefore      ##
+## encouraged to load and test the software's suitability as regards their    ##
+## requirements in conditions enabling the security of their systems and/or   ##
+## data to be ensured and,  more generally, to use and operate it in the      ##
+## same conditions as regards security.                                       ##
+##                                                                            ##
+## The fact that you are presently reading this means that you have had       ##
+## knowledge of the CeCILL-C license and that you accept its terms.           ##
+##                                                                            ##
+################################################################################
+################################################################################
 
-##################################################################################
-##################################################################################
-##                                                                              ##
-## Copyright Yoann Robin, 2020                                                  ##
-##                                                                              ##
-## yoann.robin.k@gmail.com                                                      ##
-##                                                                              ##
-## Ce logiciel est un programme informatique faisant partie de la librairie     ##
-## ROOPSD (R Object Oriented Programming for Statistical Distribution). Cette   ##
-## librarie permet de tirer aléatoirement, inférer et acceder aux fonctions CDF ##
-## et SF des distributions statistisques classiques.                            ##
-##                                                                              ##
-## Ce logiciel est régi par la licence CeCILL-C soumise au droit français et    ##
-## respectant les principes de diffusion des logiciels libres. Vous pouvez      ##
-## utiliser, modifier et/ou redistribuer ce programme sous les conditions       ##
-## de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA     ##
-## sur le site "http://www.cecill.info".                                        ##
-##                                                                              ##
-## En contrepartie de l'accessibilité au code source et des droits de copie,    ##
-## de modification et de redistribution accordés par cette licence, il n'est    ##
-## offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,    ##
-## seule une responsabilité restreinte pèse sur l'auteur du programme, le       ##
-## titulaire des droits patrimoniaux et les concédants successifs.              ##
-##                                                                              ##
-## A cet égard  l'attention de l'utilisateur est attirée sur les risques        ##
-## associés au chargement,  à l'utilisation,  à la modification et/ou au        ##
-## développement et à la reproduction du logiciel par l'utilisateur étant       ##
-## donné sa spécificité de logiciel libre, qui peut le rendre complexe à        ##
-## manipuler et qui le réserve donc à des développeurs et des professionnels    ##
-## avertis possédant  des  connaissances  informatiques approfondies.  Les      ##
-## utilisateurs sont donc invités à charger  et  tester  l'adéquation  du       ##
-## logiciel à leurs besoins dans des conditions permettant d'assurer la         ##
-## sécurité de leurs systèmes et ou de leurs données et, plus généralement,     ##
-## à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.           ##
-##                                                                              ##
-## Le fait que vous puissiez accéder à cet en-tête signifie que vous avez       ##
-## pris connaissance de la licence CeCILL-C, et que vous en avez accepté les    ##
-## termes.                                                                      ##
-##                                                                              ##
-##################################################################################
-##################################################################################
+################################################################################
+################################################################################
+##                                                                            ##
+## Copyright Yoann Robin, 2020                                                ##
+##                                                                            ##
+## yoann.robin.k@gmail.com                                                    ##
+##                                                                            ##
+## Ce logiciel est un programme informatique faisant partie de la librairie   ##
+## ROOPSD (R Object Oriented Programming for Statistical Distribution). Cette ##
+## librarie permet de tirer aléatoirement, inférer et acceder aux fonctions   ##
+## CDF et SF des distributions statistisques classiques.                      ##
+##                                                                            ##
+## Ce logiciel est régi par la licence CeCILL-C soumise au droit français et  ##
+## respectant les principes de diffusion des logiciels libres. Vous pouvez    ##
+## utiliser, modifier et/ou redistribuer ce programme sous les conditions     ##
+## de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA   ##
+## sur le site "http://www.cecill.info".                                      ##
+##                                                                            ##
+## En contrepartie de l'accessibilité au code source et des droits de copie,  ##
+## de modification et de redistribution accordés par cette licence, il n'est  ##
+## offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,  ##
+## seule une responsabilité restreinte pèse sur l'auteur du programme, le     ##
+## titulaire des droits patrimoniaux et les concédants successifs.            ##
+##                                                                            ##
+## A cet égard  l'attention de l'utilisateur est attirée sur les risques      ##
+## associés au chargement,  à l'utilisation,  à la modification et/ou au      ##
+## développement et à la reproduction du logiciel par l'utilisateur étant     ##
+## donné sa spécificité de logiciel libre, qui peut le rendre complexe à      ##
+## manipuler et qui le réserve donc à des développeurs et des professionnels  ##
+## avertis possédant  des  connaissances  informatiques approfondies.  Les    ##
+## utilisateurs sont donc invités à charger  et  tester  l'adéquation  du     ##
+## logiciel à leurs besoins dans des conditions permettant d'assurer la       ##
+## sécurité de leurs systèmes et ou de leurs données et, plus généralement,   ##
+## à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.         ##
+##                                                                            ##
+## Le fait que vous puissiez accéder à cet en-tête signifie que vous avez     ##
+## pris connaissance de la licence CeCILL-C, et que vous en avez accepté les  ##
+## termes.                                                                    ##
+##                                                                            ##
+################################################################################
+################################################################################
 
 #' rv_histogram 
 #'
 #' rv_histogram distribution in OOP way. Use quantile.
-#'
-#' @docType class
-#' @importFrom R6 R6Class
-#'
-#' @param n  [integer]
-#'        For rvs function, number of samples drawn
-#' @param x  [integer]
-#'        For density and logdensity function. Vector of quantiles
-#' @param q  [integer]
-#'        For cdf and sf functions. Vector of quantiles
-#' @param p  [integer]
-#'        For icdf and isf functions. Vector of probabilities
-#' @param Y  [vector]
-#'        For fit function. Dataset to infer parameters. (Quantile)
-#' @param bins [integer]
-#'        Numbers of quantiles to estimate between 0 and 1.
-#' @return Object of \code{\link{R6Class}} with methods to use a statistical distribution.
-#' @format \code{\link{R6Class}} object.
-#'
-#' @section Methods:
-#' \describe{
-#'   \item{\code{new()}}{This method is used to create object of this class with \code{rv_histogram}}
-#'   \item{\code{rvs(n)}}{Draw n samples}.
-#'   \item{\code{cdf(q)}}{Cumulative Distribution Function along vector of quantile q}.
-#'   \item{\code{sf(q)}}{Survival function (1-CDF) along vector of quantile q}.
-#'   \item{\code{icdf(p)}}{Inverse of cdf along vector of probabilities p}.
-#'   \item{\code{isf(p)}}{Inverse of sf along vector of probabilities p}.
-#'   \item{\code{fit(Y,bins)}}{Fit function to infer parameters}.
-#' }
-#' @examples
-#' ##
-#' ##
-#' @export
 rv_histogram = R6::R6Class( "rv_histogram" ,
 	
 	public = list(
@@ -126,16 +93,25 @@ rv_histogram = R6::R6Class( "rv_histogram" ,
 	## Arguments ##
 	###############
 	
+	#' @field min min value for the estimation
 	min   = NULL,
+	#' @field max max value for the estimation
 	max   = NULL,
+	#' @field freeze not used currently
 	freeze = FALSE,
+	#' @field tol numerical tolerance
 	tol = 1e-2,
 	
 	#################
 	## Constructor ##
 	#################
 	
-	initialize = function(...)##{{{
+	## initialize ##{{{
+	#' @description
+    #' Create a new rv_histogram object.
+    #' @param ... If a param `Y`is given, the fit method is called with `...`.
+    #' @return A new `rv_histogram` object.
+	initialize = function(...)
 	{
 		kwargs = list(...)
 		if( !is.null(kwargs[["Y"]]) )
@@ -143,38 +119,80 @@ rv_histogram = R6::R6Class( "rv_histogram" ,
 	},
 	##}}}
 	
-	rvs = function( n )##{{{
+	## rvs ##{{{
+	#' @description
+    #' Generation sample from the histogram
+    #' @param n Number of samples drawn
+    #' @return A vector of samples
+	rvs = function( n )
 	{
 		p = stats::runif( n , 0 , 1 )
 		return(self$icdf(p))
 	},
 	##}}}
 	
-	cdf = function( q )##{{{
+	## cdf ##{{{
+	#' @description
+    #' Cumulative Distribution Function
+    #' @param q Quantiles to compute the CDF
+    #' @return cdf values
+	cdf = function( q )
 	{
 		return(private$cdffn(q))
 	},
 	##}}}
 	
-	icdf = function( p )##{{{
+	## icdf ##{{{
+	#' @description
+    #' Inverse of Cumulative Distribution Function
+    #' @param p Probabilities to compute the CDF
+    #' @return icdf values
+	icdf = function( p )
 	{
 		return(private$icdffn(p))
 	},
 	##}}}
 	
-	sf = function( q ) ##{{{
+	## sf ##{{{
+	#' @description
+    #' Survival Function
+    #' @param q Quantiles to compute the SF
+    #' @return sf values
+	sf = function( q ) 
 	{
 		return( 1. - private$cdffn(q) )
 	},
 	##}}}
 	
-	isf = function( p ) ##{{{
+	## isf ##{{{
+	#' @description
+    #' Inverse of Survival Function
+    #' @param p Probabilities to compute the SF
+    #' @return isf values
+	isf = function( p ) 
 	{
 		return(private$icdffn(1. - p))
 	},
 	##}}}
 	
-	fit = function( Y , bins = as.integer(100) )##{{{
+	## density ##{{{
+	#' @description
+    #' Density function
+    #' @param x Values to compute the density
+    #' @return density
+	density = function( x ) 
+	{
+		return(private$densityfn(x))
+	},
+	##}}}
+	
+	## fit ##{{{
+	#' @description
+    #' Fit method for the histograms
+    #' @param Y Dataset to infer the histogram
+    #' @param bins bins values, can be a vector or an integer
+    #' @return `self`
+	fit = function( Y , bins = as.integer(100) )
 	{
 		self$min = base::min(Y)
 		self$max = base::max(Y)
@@ -195,6 +213,14 @@ rv_histogram = R6::R6Class( "rv_histogram" ,
 		}
 		quants = private$cdffn(x)
 		private$icdffn = stats::approxfun( quants , x , yleft = self$min - delta , yright = self$max + delta )
+		
+		## Density function
+		hist = graphics::hist( X , breaks = x , plot = FALSE )
+		p = hist$density / base::sum(hist$density)
+		c = hist$mids
+		private$densityfn = stats::approxfun( c , p , yleft = 0 , yright = 0 )
+		
+		return(self)
 	}
 	##}}}
 	
@@ -211,8 +237,9 @@ rv_histogram = R6::R6Class( "rv_histogram" ,
 	## Arguments ##
 	###############
 	
-	cdffn  = NULL,
-	icdffn = NULL
+	cdffn     = NULL,
+	icdffn    = NULL,
+	densityfn = NULL
 	
 	
 	)
