d023e4da26c0ef56ad55b030bc5fdc0858ba2624
Yoann Robin <yoann.robin.k@gmail.com>
2020-08-04 12:09:49 +0200

    Bugfix: bad construction of density

--- ROOPSD/R/dist.rv_histogram.R
+++ ROOPSD/R/dist.rv_histogram.R
@@ -236,8 +236,8 @@ rv_histogram = R6::R6Class( "rv_histogram" ,
 		private$icdffn = stats::approxfun( quants , x , yleft = self$min - delta , yright = self$max + delta )
 		
 		## Density function
-		hist = graphics::hist( X , breaks = x , plot = FALSE )
-		p = hist$density / base::sum(hist$density)
+		hist = graphics::hist( Y , freq = FALSE , breaks = x , plot = FALSE )
+		p = hist$density #/ base::sum(hist$density)
 		c = hist$mids
 		private$densityfn = stats::approxfun( c , p , yleft = 0 , yright = 0 )
 		
